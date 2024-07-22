7f3255312ea24374f5867c6bd94eedb4391816df
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-22 17:53:46 -0300

    Css in Js

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles.js"
+
+export function Details() {
+  
+  return (
+    <Container>
+      <h1>Teste</h1>
+      <span>Segundo teste</span>
+    </Container>
+  )
+}

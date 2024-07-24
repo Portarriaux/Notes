87ecbe75c3cb9904a3d275ae3685697ced8dd271
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-24 17:53:50 -0300

    Criando componente de Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,30 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag} from "../../components/Tags";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.gitHub.Portarriaux.com.br</a></li>
+          <li><a href="#">https://www.gitHub.Portarriaux.com.br</a></li>
+          
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="NodeJs"/>
+        <Tag title="Javascript"/>
+      </Section>
+
       <Button title="Login" loading />
-      <Button title="Cadastrar" />
-      <Button title="Voltar" />
+     
     </Container>
   );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -11,3 +11,17 @@ export const Container = styled.div`
     "content";
   border: solid red 2px;
 `;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+`;
+
+

ecfb763d27a0adad24a98e1b898ab4985bcd475f
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-24 15:47:39 -0300

    Adicionando compneente de Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import {RiShutDownLine} from "react-icons/ri"
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+    return(
+        <Container>
+            <Profile>
+                <img 
+                    src="https://github.com/Portarriaux.png"
+                    alt="Foto do usuário"
+                />
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Jhonatan Portarriaux</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    )
+}

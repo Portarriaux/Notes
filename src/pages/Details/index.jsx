b21e13503af72206f1b2bda33ec6ac6ea1f4023f
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-24 15:48:45 -0300

    Atualizações

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,16 @@
-import { Container } from './styles'
+import { Container } from "./styles";
 
-import { Button } from '../../components/Button'
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Rodrigo Gonçalves</span>
+      <Header />
 
       <Button title="Login" loading />
       <Button title="Cadastrar" />
       <Button title="Voltar" />
     </Container>
-  )
+  );
 }

9f7e04c5ca48e6337ab61fb3cc778a10ee0e9d11
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-25 16:19:26 -0300

    Finalizando Details

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -13,7 +13,6 @@ export const Container = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border: solid blue 2px;
 `;
 
 export const Profile = styled.div`
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,30 +1,52 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
-import { Tag} from "../../components/Tags";
+import { Tag } from "../../components/Tags";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.gitHub.Portarriaux.com.br</a></li>
-          <li><a href="#">https://www.gitHub.Portarriaux.com.br</a></li>
-          
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução ao React</h1>
 
-      <Section title="Marcadores">
-        <Tag title="NodeJs"/>
-        <Tag title="Javascript"/>
-      </Section>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
 
-      <Button title="Login" loading />
-     
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.gitHub.Portarriaux.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.gitHub.Portarriaux.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="NodeJs" />
+            <Tag title="Javascript" />
+          </Section>
+
+          <Button title="Login" loading />
+        </Content>
+      </main>
     </Container>
   );
 }

ae7ec20f347e5e89a18ea3d2fe4d52605712e3e5
Portarriaux <jhonatancaetano14@hotmail.com>
2024-07-26 17:30:40 -0300

    Adiciona componente Input com suporte a ícone no projeto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
   return (
@@ -9,21 +12,25 @@ export function Home() {
         <h1>Rocketnotes</h1>
       </Brand>
 
-      <Header></Header>
+      <Header />
 
       <Menu>
-
+      <li><ButtonText title="Todos" $isactive /></li>
+      <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Nodejs" /></li>
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título" />
       </Search>
 
       <Content>
+
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
     </Container>
   )

230e034d87e2b52bc9ffc49bd9c2b7f609fe5f22
Portarriaux <jhonatancaetano14@hotmail.com>
2024-09-18 10:45:01 -0300

    Modificando token

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,22 @@
-import {RiShutDownLine} from "react-icons/ri"
+import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout } from "./styles";
-import { useAuth } from "../../hooks/auth"
-
+import { useAuth } from "../../hooks/auth";
 
 export function Header() {
-    const { signOut } = useAuth();
-    return(
-        <Container>
-            <Profile to="/profile">
-                <img 
-                    src="https://github.com/Portarriaux.png"
-                    alt="Foto do usuário"
-                />
-                <div>
-                    <span>Bem-Vindo</span>
-                    <strong>Jhonatan Portarriaux</strong>
-                </div>
-            </Profile>
+  const { signOut } = useAuth();
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img src="https://github.com/Portarriaux.png" alt="Foto do usuário" />
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Jhonatan Portarriaux</strong>
+        </div>
+      </Profile>
 
-            <Logout onClick={signOut}>
-                <RiShutDownLine/>
-            </Logout>
-
-        </Container>
-    )
+      <Logout onClick={signOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
 }
